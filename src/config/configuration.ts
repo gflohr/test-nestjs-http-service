fb050a91dd995d5aae4e10167dab669854bc8004
Guido Flohr <guido.flohr@cantanea.com>
2024-01-11 17:47:05 +0200

    add configuration

---
+++ src/config/configuration.ts
@@ -0,0 +1,7 @@
+const DEFAULT_UNIVERSITIES_BASE_URL = 'http://universities.hipolabs.com';
+
+export default () => ({
+	universities: {
+		baseUrl: process.env.UNIVERSITIES_BASE_URL || DEFAULT_UNIVERSITIES_BASE_URL,
+	},
+});
